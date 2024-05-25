2fabb929641630b25cbec4003111b0f129e4c099
icmauro <italo_mauro2000@yahoo.com.br>
2024-05-24 21:31:26 -0300

    entregando projeto

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -13,18 +15,46 @@ namespace DesafioProjetoHospedagem.Models
             DiasReservados = diasReservados;
         }
 
+        public void ValorDiaria()
+        {
+            Console.WriteLine($"Diária: {Suite.ValorDiaria}");
+
+        }
+
+        public void DiasDeReserva()
+        {
+            Console.WriteLine($"Dias Reservados: {DiasReservados}");
+
+        }
+
+        public decimal valorTotal()
+        { 
+            return DiasReservados * Suite.ValorDiaria;
+        }
+
+        public void DescontoValor()
+        {
+            Console.WriteLine($"Desconto de 10% sobre o valor total { valorTotal() }. Pois a reserva foi igual/acima de 10 dias ");
+            Console.WriteLine("------------------------------------");
+        }
+
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+
+            var capacidade = hospedes.Count <= Suite.Capacidade; 
+
+            if (capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // *IMPLEMENTE AQUI*         
+
+                throw new Exception($"Capacidade não permitida, somente acima de {Suite.Capacidade}.");
             }
         }
 
@@ -37,6 +67,15 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
+
+            foreach (var hospede in Hospedes)
+            {
+                Console.WriteLine($"Hospede: {hospede.NomeCompleto}" );
+            }
+
+            Console.WriteLine($"Total Hospedes: { Hospedes.Count }");
+            Console.WriteLine("------------------------------------");
+
             return 0;
         }
 
@@ -45,13 +84,17 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = valorTotal();
+
+            decimal desconto =  (0.1M * valor) ;
+
+            bool totalDiasReservados = DiasReservados >= 10;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (totalDiasReservados)
             {
-                valor = 0;
+                valor = valor - desconto;
             }
 
             return valor;
